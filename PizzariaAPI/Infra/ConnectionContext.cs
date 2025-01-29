48641f1b56255d6e6f38612dac0754deaecc48c3
Nayla Hilana da Silva <hilanacs@outlook.com>
2025-01-29 17:00:11 -0300

    feat: configura o contexto do banco de dados

---
+++ PizzariaAPI/Infra/ConnectionContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzariaAPI.Domain.Models;
+
+namespace PizzariaAPI.Infra
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        // Configuração do banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "Server=localhost;" +
+                "Port=3306;" +
+                "Database=pizzaria_db;" +
+                "User Id=root;" +
+                "Password=admin;",
+                new MySqlServerVersion(new Version(8, 0, 41))
+            );
+        }
+    }
+}
